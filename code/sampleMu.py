5518dfa050cd10ea580bf31ee7725099f081ff2e
Christian <s194255@student.dtu.dk>
2021-06-21 16:41:58 +0200

    commented sample_from_NF and sampleMu

--- code/sampleMu.py
+++ code/sampleMu.py
@@ -1,4 +1,4 @@
-from priors import noRoot_gauss, Root_gauss, noRoot_NF, Root_NF, zeroPrior
+from priors import noRoot_gauss, noRoot_NF, zeroPrior
 import AMCParsermaster.amc_parser_pytorch as amc
 import glob
 import torch
@@ -8,44 +8,57 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 K = 7
 flowtype = "MAF"
-nitts = 10
-saveRoot = "../muSamples"
+nitts = 1
+saveRoot = ""
 
+# if the path don't exist, make it!
 if os.path.isdir(saveRoot) == False:
     os.makedirs(saveRoot)
-dataPaths = {"walking": (glob.glob('../data/subjects/Walking/*.amc'),
-                             '../data/subjects/Walking/35.asf'),
-                 "running": (glob.glob('../data/subjects/Running/*.amc'),
-                             '../data/subjects/Running/16.asf'),
-                 "boxing": (glob.glob('../data/subjects/Boxing/*.amc'),
-                            '../data/subjects/Boxing/14.asf')}
+dataPaths = {"walking": (glob.glob('../data/Walking/*.amc'),
+                             '../data/Walking/35.asf'),
+                 "running": (glob.glob('../data/Running/*.amc'),
+                             '../data/Running/16.asf'),
+                 "boxing": (glob.glob('../data/Boxing/*.amc'),
+                            '../data/Boxing/14.asf')}
 
 def saveMotion(motion, path, fileName):
+    """
+    :param motion: A joint angle dictionary
+    :param path: A path (wihtout filename)
+    :param fileName: The filename
+    :return:
+    """
+    # cloning the joint angle dictionary
     motionClone =  {}
     for key in motion:
         tensorclone = motion[key].clone()
         motionClone[key] = tensorclone.detach().cpu()
+    # if the path doesnt exist already, create it
     if os.path.isdir(path):
         pass
     else:
         print("direc no exist. Creating it ...")
         os.makedirs(path)
+    # dump joint angle dictionary is pickle
     with open(os.path.join(path, fileName), "wb") as f:
         pickle.dump(motionClone, f)
 
-for bevægelse in dataPaths:
-    amc_filer, asf_fil = dataPaths[bevægelse]
+for activity in dataPaths:
+    amc_filer, asf_fil = dataPaths[activity]
     frameslist = amc.getFrameslist(amc_filer)
     joints = amc.parse_asf(asf_fil)
     prior = noRoot_NF(frameslist=frameslist, K=K, flowtype=flowtype, nitts=nitts)
 
-    #WE WANNA SEE THAT MU VEC
-    vector = prior.mu
-    #NORMALIZE MU VEC
-    vector = (vector - prior.mu)/prior.stds
+    # Get the mean vector of the NF's prior (it's usually the Standard Gauss)
+    vector = prior.NF_model.prior.loc
+    # Shape needs to be (1, 53), not (53,)
+    vector = vector.unsqueeze(dim=0)
 
+    #let the vector go in the sample direction
     samples =  prior.sampleDirection(vector)
 
-    saveMotion(samples[0], saveRoot, "{}_mu.pkl".format(bevægelse))
+    # save the joint angles as pickle
+    saveMotion(samples[0], saveRoot, "{}_mu.pkl".format(activity))
     joints["root"].set_motion(samples[0])
-    joints["root"].draw(savePath=os.path.join(saveRoot, "{}_mu.jpg".format(bevægelse)))
+    # save a snapshot of the pose
+    joints["root"].draw(savePath=os.path.join(saveRoot, "{}_mu.jpg".format(activity)))
--- code/sample_from_NF.py
+++ code/sample_from_NF.py
@@ -1,4 +1,4 @@
-from priors import noRoot_gauss, Root_gauss, noRoot_NF, Root_NF, zeroPrior
+from priors import noRoot_gauss, noRoot_NF, zeroPrior
 import AMCParsermaster.amc_parser_pytorch as amc
 import glob
 import torch
@@ -10,41 +10,53 @@ K = 7
 flowtype = "MAF"
 nitts = 1
 num_samples = 5
-saveRoot = "../samples2"
+saveRoot = ""
 
 
 def saveMotion(motion, path, fileName):
+    """
+    :param motion: A joint angle dictionary
+    :param path: A path (wihtout filename)
+    :param fileName: The filename
+    :return:
+    """
+    # cloning the joint angle dictionary
     motionClone =  {}
     for key in motion:
         tensorclone = motion[key].clone()
         motionClone[key] = tensorclone.detach().cpu()
+    # if the path doesnt exist already, create it
     if os.path.isdir(path):
         pass
     else:
         print("direc no exist. Creating it ...")
         os.makedirs(path)
+    # dump joint angle dictionary is pickle
     with open(os.path.join(path, fileName), "wb") as f:
         pickle.dump(motionClone, f)
 
 
-dataPaths = {"walking": (glob.glob('../data/subjects/Walking/*.amc'),
-                             '../data/subjects/Walking/35.asf'),
-                 "running": (glob.glob('../data/subjects/Running/*.amc'),
-                             '../data/subjects/Running/16.asf'),
-                 "boxing": (glob.glob('../data/subjects/Boxing/*.amc'),
-                            '../data/subjects/Boxing/14.asf')}
+dataPaths = {"walking": (glob.glob('../data/Walking/*.amc'),
+                             '../data/Walking/35.asf'),
+                 "running": (glob.glob('../data/Running/*.amc'),
+                             '../data/Running/16.asf'),
+                 "boxing": (glob.glob('../data/Boxing/*.amc'),
+                            '../data/Boxing/14.asf')}
 
 
 
-for bevægelse in dataPaths:
-    amc_filer, asf_fil = dataPaths[bevægelse]
+for activity in dataPaths:
+    amc_filer, asf_fil = dataPaths[activity]
     frameslist = amc.getFrameslist(amc_filer)
     joints = amc.parse_asf(asf_fil)
     prior = noRoot_NF(frameslist=frameslist, K=K, flowtype=flowtype, nitts=nitts)
     samples =  prior.sample(num_samples=num_samples)
 
     for i in range(len(samples)):
+        #make the rotational coordinates zero.
         samples[i]["root"][3:6] = torch.tensor([0.0,0.0,0.0], device=device)
-        saveMotion(samples[i], os.path.join(saveRoot, bevægelse), "{}.pkl".format(i))
+        #save the joint angle vector
+        saveMotion(samples[i], os.path.join(saveRoot, activity), "{}.pkl".format(i))
         joints["root"].set_motion(samples[i])
-        joints["root"].draw(savePath=os.path.join(saveRoot, bevægelse, "{}.jpg".format(i)))
+        #save a snapshot of pose
+        joints["root"].draw(savePath=os.path.join(saveRoot, activity, "{}.jpg".format(i)))
